ff9d323737543b3698c4f84135425305f99d0d4e
JeCkaV <jeckavucko@gmail.com>
2022-03-20 13:44:27 +0100

    Creating TO DO list APP
    Creating HTML and CSS files as base for app, also creating JS using jQuery instead of vanilla JS

---
+++ script.js
@@ -0,0 +1,40 @@
+function newItem() {
+    
+    // Adding a new item to the list of items
+
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === ' ') {
+        alert("You must write something!");
+    } else {
+        $('#list').append(li);
+    }
+
+    // Crossing out an item from the list of items with double click
+     
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
+    });
+
+    // Adding the delete button "X" with event listener when we click it to be deleted 
+    // and Adding CLASS DELETE (DISPLAY: NONE) from the css
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.appent(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+
+    function deleteListItem() {
+        li.addClass("delete")
+    }
+
+    // Reordering the items
+    $('#list').sortable();
+}
